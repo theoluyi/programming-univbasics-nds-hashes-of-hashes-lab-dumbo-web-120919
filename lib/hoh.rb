7f9fadacbef099ed6811f5e7da2ac550d812cb25
Theo Carney <theoluyi@gmail.com>
2019-12-04 23:00:54 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -35,7 +35,11 @@ def naming_system
   label: "Kingdom",
   sub_category: { label: "Phylum",
     sub_category: { label: "Class",
-      sub_category: { label: "Order"
+      sub_category: { label: "Order",
+        sub_category: { label: "Family",
+        
+          
+        }
       }
     }
   }
