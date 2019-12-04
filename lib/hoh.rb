ef0bbac73d200e7b0a9c30ae04a52aa5ef37327b
Theo Carney <theoluyi@gmail.com>
2019-12-04 23:01:29 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -37,8 +37,8 @@ def naming_system
     sub_category: { label: "Class",
       sub_category: { label: "Order",
         sub_category: { label: "Family",
-        
-          
+          sub_category: { label: "Genus",
+          }
         }
       }
     }
